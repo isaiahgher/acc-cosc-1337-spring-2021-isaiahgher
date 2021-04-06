316c5e9b0fde8091e7491b3745ee10905d32b20e
isaiahgher <isaiah.hernandez2@g.austincc.edu>
2021-04-06 17:52:39 -0500

    bank account

--- src/examples/06_module/01_bank/main.cpp
+++ src/examples/06_module/01_bank/main.cpp
@@ -1,21 +1,42 @@
-#include<iostream>
-using std::cout;
-
+#include "atm.h"
 #include "bank_account.h"
+#include<iostream>
+#include<time.h>
+using std::cout; using std::cin;
+
 
 int main()
 {
-	BankAccount account = get_account(100);//variable-an instance of BankAccount object in memory
+	srand(time(NULL));
+	BankAccount account = get_account(100);
+	BankAccount a1(100), a2(100);
+	BankAccount a3 = a1 + a2;
+	cout<<a3;
+//	display_balance(account);
 	
-	display_account(account);
-
+	/*char choice;
+	ATM atm;
+	do
+	{
+		atm.scan_card();
+		atm.display_balance();
+		cout<<"Continue y?";
+		cin>>choice;
+	}while(choice == 'y' || choice == 'Y');
+	friend_display_balance(account);
+	display_balance(account);
+	
+//	cout<<sizeof(account)<<"\n";
+	auto balance = account.get_balance();
+	cout<<"Balance: "<<balance<<"\n";
 	cout<<"Deposit $50\n";
 	account.deposit(50);
-	cout<<"Balance: "<<account.get_balance()<<"\n";
-
+	balance = account.get_balance();
+	cout<<"Balance: "<<balance<<"\n\n";
 	cout<<"Withdraw $10\n";
 	account.withdraw(10);
-	cout<<"Balance: "<<account.get_balance()<<"\n";
+	balance = account.get_balance();
+	cout<<"Balance: "<<balance<<"\n";*/
 
 	return 0;
 }
