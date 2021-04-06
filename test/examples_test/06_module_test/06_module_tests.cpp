9d9c591fa3362ff5910dae11f3fdc03728d51358
isaiahgher <isaiah.hernandez2@g.austincc.edu>
2021-04-06 17:53:13 -0500

    bank account test case

--- test/examples_test/06_module_test/06_module_tests.cpp
+++ test/examples_test/06_module_test/06_module_tests.cpp
@@ -5,53 +5,98 @@
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
+
 TEST_CASE("Test new bank account object balance")
 {
 	BankAccount account;
 
-	REQUIRE(account.get_balance()==0);
+	REQUIRE(account.get_balance() == 0);
 }
+
+TEST_CASE("Test static bank_balance top")
+{
+	BankAccount account;
+	account.deposit(50);
+
+	REQUIRE(account.get_bank_balance() == 50);
+}
+
+TEST_CASE("Test new bank account with given begin balance")
+{
+	BankAccount account(100);
+
+	REQUIRE(account.get_balance() == 100);
+}
+
 TEST_CASE("Test bank account deposit")
 {
 	BankAccount account;
 	account.deposit(50);
 
-	REQUIRE(account.get_balance()==50);
+	REQUIRE(account.get_balance() == 50);
+
 }
+
+TEST_CASE("Test bank account deposit with given begin balance")
+{
+	BankAccount account(100);
+
+	account.deposit(50);
+	REQUIRE(account.get_balance() == 150);
+}
+
 TEST_CASE("Test bank account deposit with negative amount")
 {
 	BankAccount account;
 	account.deposit(50);
-	REQUIRE(account.get_balance()==50);
+	REQUIRE(account.get_balance() == 50);
 
 	account.deposit(-10);
-	REQUIRE(account.get_balance()==50);
-
+	REQUIRE(account.get_balance() == 50);
 }
+
 TEST_CASE("Test bank account withdraw")
 {
 	BankAccount account;
 	account.deposit(50);
-	REQUIRE(account.get_balance()==50);
+	REQUIRE(account.get_balance() == 50);
 
 	account.withdraw(10);
-	REQUIRE(account.get_balance()==40);
+	REQUIRE(account.get_balance() == 40);
 }
-TEST_CASE("Test bank account withdraw with negative amount")
+
+TEST_CASE("Test bank account withdraw with a given begin balance")
+{
+	BankAccount account(100);
+	account.withdraw(10);
+
+	REQUIRE(account.get_balance() == 90);
+}
+
+TEST_CASE("Test Bank account withdraw with negative amount")
 {
 	BankAccount account;
 	account.deposit(50);
-	REQUIRE(account.get_balance()==50);
+	REQUIRE(account.get_balance() == 50);
 
 	account.withdraw(-10);
-	REQUIRE(account.get_balance()==50);
+	REQUIRE(account.get_balance() == 50);
 }
-TEST_CASE("Test bank account withdraw with NSF")
+
+TEST_CASE("Test Bank account withdraw with NSF")
 {
 	BankAccount account;
 	account.deposit(50);
-	REQUIRE(account.get_balance()==50);
+	REQUIRE(account.get_balance() == 50);
 
 	account.withdraw(51);
-	REQUIRE(account.get_balance()==50);
+	REQUIRE(account.get_balance() == 50);
+}
+
+TEST_CASE("Test static bank_balance")
+{
+	BankAccount account;
+	account.deposit(50);
+
+	REQUIRE(account.get_bank_balance() == 680);
 }
